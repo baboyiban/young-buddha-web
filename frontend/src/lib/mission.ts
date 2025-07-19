5315e32f8b754829bfcb322dc20f4ceeef74f62b
baboyiban <chl11wq12@gmail.com>
2025-07-19 21:12:45 +0900

    fix: style

--- frontend/src/lib/mission.ts
+++ frontend/src/lib/mission.ts
@@ -29,7 +29,7 @@ export async function loadHomeSheetData() {
     const items = firstRow.map((cell: string) => cell && cell !== '-' ? cell : '');
 
     const html = `
-    <div class="flex flex-col items-center justify-center h-full
+    <div class="flex flex-col items-center justify-center-safe h-full
     *:mb-[1rem] *:text-center *:*:mb-[0.5rem]
     ">
       <div class="">🌴${items[0]} ${items[1]}요일 청년붓다 소임🌴</div>
