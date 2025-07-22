c01ba6ae8de28926994a23ca0aede4c357ce74d0
baboyiban <chl11wq12@gmail.com>
2025-07-22 17:51:57 +0900

    test: 구글 권한에서 스프레드시트 권한 제거해봄

--- backend/src/config/constants.zig
+++ backend/src/config/constants.zig
@@ -1,4 +1,4 @@
 pub const SESSION_COOKIE_NAME = "session";
 pub const OAUTH_STATE_COOKIE_NAME = "oauth_state";
 pub const STATIC_FILES_PATH_KEY = "STATIC_FILES_PATH";
-pub const GOOGLE_SCOPE = "openid email profile https://www.googleapis.com/auth/spreadsheets.readonly";
+pub const GOOGLE_SCOPE = "openid email profile";
