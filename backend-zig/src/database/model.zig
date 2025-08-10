595329313fbd51ccc7685a42213e0ee42b4aeac1
baboyiban <chl11wq12@gmail.com>
2025-08-10 15:12:32 +0900

    refactor: backen-rs -> backend

--- backend-zig/src/database/model.zig
+++
@@ -1,9 +0,0 @@
-pub const DatabaseRequest = struct {
-    id: i64,
-    name: []const u8,
-    type: []const u8,
-    request_date: []const u8,
-    absent_date: ?[]const u8,
-    partial_schedule: ?[]const u8,
-    reason: ?[]const u8,
-};
--- backend-zig/src/sheets/model.zig
+++
@@ -1,6 +0,0 @@
-const std = @import("std");
-
-pub const QueryRequest = struct {
-    spreadsheet_id: []const u8,
-    query: []const u8,
-};
