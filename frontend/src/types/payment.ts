49e938ab2da078af9d662438e2b5e1c141e79d2e
baboyiban <chl11wq12@gmail.com>
2025-08-23 17:27:48 +0900

    feat: 사용자 이메일 기반 이름 조회 기능 추가 및 결재 요청 데이터 구조 확장

--- frontend/src/types/payment.ts
+++ frontend/src/types/payment.ts
@@ -1,10 +1,12 @@
 export interface PaymentRequest {
-  id: string
-  name: string
-  requestDate: string
-  type: string
-  absentDate: string
-  schedule: string
-  reason: string
-  approved: string
+  id: string;
+  email: string;
+  userId: string;
+  name: string;
+  requestDate: string;
+  type: string;
+  absentDate: string;
+  schedule: string;
+  reason: string;
+  approved: string;
 }
