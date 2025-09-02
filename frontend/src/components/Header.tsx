163be064907dc55c2681f4fa5db7b9dabffa1416
baboyiban <chl11wq12@gmail.com>
2025-09-02 18:07:52 +0900

    style: Header 컴포넌트의 스타일 및 구조 개선

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -14,15 +14,19 @@ export default function Header() {
   };
 
   return (
-    <header className="flex justify-between items-center p-[0.5rem]">
+    <header className="h-[52px] flex justify-between items-center p-[0.5rem]">
       <Link href="/" className="text-xl font-bold mx-[0.5rem] !flex items-center space-x-[0.25rem]">
         <Image alt="logo" src="/favicon.ico" width={24} height={24} /> <p>청년붓다</p>
       </Link>
       <div className="flex items-center space-x-2">
-        {user && <span className="text-sm text-gray-50">{user.name}</span>}
-        <button onClick={handleLogout} className="button red text-sm">
-          로그아웃
-        </button>
+        {user && (
+          <>
+            <span className="text-sm text-gray-50">{user.name}</span>
+            <button onClick={handleLogout} className="button red text-sm">
+              로그아웃
+            </button>
+          </>
+        )}
       </div>
     </header>
   );
