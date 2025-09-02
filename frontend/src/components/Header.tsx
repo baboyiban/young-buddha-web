26c4032137f9a508f55eede80a5ef8bf9b6839ce
baboyiban <chl11wq12@gmail.com>
2025-09-02 18:03:16 +0900

    feat: 헤더에 로고 이미지 추가

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import { useAuth } from "@/lib/hooks/useAuth";
+import Image from "next/image";
 
 export default function Header() {
   const { user, logout } = useAuth();
@@ -14,8 +15,8 @@ export default function Header() {
 
   return (
     <header className="flex justify-between items-center p-[0.5rem]">
-      <Link href="/" className="text-xl font-bold mx-[0.5rem]">
-        🪷 청년붓다
+      <Link href="/" className="text-xl font-bold mx-[0.5rem] !flex items-center space-x-[0.25rem]">
+        <Image alt="logo" src="/favicon.ico" width={24} height={24} /> <p>청년붓다</p>
       </Link>
       <div className="flex items-center space-x-2">
         {user && <span className="text-sm text-gray-50">{user.name}</span>}
